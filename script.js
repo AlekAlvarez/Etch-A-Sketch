96a4a5666efd9c3b9efb1498ecd2014b8ea8f5cf
Aleksander Alvarez <alekalvarez123@gmail.com>
2024-04-20 11:12:40 -0500

    Implements resize button

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-const container=document.querySelector("#big-container");
-for(let i=0;i<16;i++){
-    const row=document.createElement("div");
-    row.className="row";
-    for(let j=0;j<16;j++){
-        const box=document.createElement("div");
-        box.className="box";
-        row.appendChild(box);
-        box.addEventListener("mouseenter",(event)=>{
-            event.target.style.backgroundColor="black";
-        })
+let container=document.querySelector("#big-container");
+const body=document.querySelector("body");
+function box(size=16){
+    for(let i=0;i<size;i++){
+        const row=document.createElement("div");
+        row.className="row";
+        for(let j=0;j<size;j++){
+            const box=document.createElement("div");
+            box.className="box";
+            row.appendChild(box);
+            box.addEventListener("mouseenter",(event)=>{
+                event.target.style.backgroundColor="black";
+            })
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
+box();
+const button=document.querySelector("button");
+button.addEventListener("click",()=>{
+   let size= prompt("How many squares wide and long should it be?");
+   if(isNaN(size*1)){
+    alert("Invalid input");
+    return;
+   }
+   container.remove();
+   container=document.createElement("div");
+   container.id="big-container";
+   body.appendChild(container);
+    box(size);
+});
